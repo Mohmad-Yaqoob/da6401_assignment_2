22e43615f5a6081b2a62a9e55d7c5b20509e12ec
Mohmad-Yaqoob <yaqoobnits25@gmail.com>
2026-04-04 03:33:15 +0000

    fix multitask class name and pixel space localization

--- models/__init__.py
+++ models/__init__.py
@@ -2,12 +2,12 @@ from models.layers import CustomDropout
 from models.vgg11 import VGG11
 from models.localization import LocalizationModel
 from models.segmentation import SegmentationModel
-from models.multitask import MultiTaskModel
+from models.multitask import MultiTaskPerceptionModel
 
 __all__ = [
     "CustomDropout",
     "VGG11",
     "LocalizationModel",
     "SegmentationModel",
-    "MultiTaskModel",
+    "MultiTaskPerceptionModel",
 ]
